9a91a157ab9f1fa580444bb8249ede4a87ba2ca5
Ben Marshall <ben.marshall@bristol.ac.uk>
2018-11-26 11:30:26 +0000

    Add XCrypto implementation of mpn_add
     On branch dev/ben-marshall
     Changes to be committed:
    	modified:   src/mp/mpn/mpn_add.h
    	new file:   src/mp/mpn/mpn_add_xc.X

--- src/mp/mpn/mpn_add.h
+++ src/mp/mpn/mpn_add.h
@@ -3,6 +3,8 @@
 
 #include  "mpn.h"
 
+extern limb_t mpn_add_xc ( limb_t* r, const limb_t* x, int l_x, const limb_t* y, int l_y );
+
 extern limb_t mpn_add  ( limb_t* r, const limb_t* x, int l_x, const limb_t* y, int l_y );
 
 extern limb_t mpn_add_1( limb_t* r, const limb_t* x, int l_x, const limb_t  y          );
