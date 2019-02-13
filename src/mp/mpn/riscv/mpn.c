917bdc795e89c6a8908f2bb825222c70b0f28131
phthinh <hung3@e.ntu.edu.sg>
2019-02-13 16:24:26 +0000

    add mpn_mul_1 and mpn_mul_n functions

--- src/mp/mpn/riscv/mpn.c
+++ src/mp/mpn/riscv/mpn.c
@@ -7,3 +7,12 @@ int mpn_lop( const limb_t* x, int l_x ) {
 
   return l_x;
 }
+
+void mpn_mul_1( limb_t* r, const limb_t* x, int l_x,  const limb_t  y        ) {
+  mpn_mul( r, x, l_x, &y, 1 );
+}
+
+void mpn_mul_n( limb_t* r, const limb_t* x,           const limb_t* y, int l ) {
+  mpn_mul( r, x, l,    y, l );
+}
+
