6b491389a02e47790cd232d611315bd918b447b2
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-02-13 14:44:45 +0000

    Fix AES test - wrong functional call signature
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   test/aes/test_aes.c

--- test/aes/test_aes.c
+++ test/aes/test_aes.c
@@ -21,6 +21,8 @@ void test_aes( int n ) {
   #if defined( CONF_AES_ENABLE_ENC )
   for( int i = 0; i < n; i++ ) {
     uint8_t c[ 16 ], m[ 16 ], k[ 16 ];
+    
+    printf("# aes dec %d / %d\n", i,n);
 
     test_aes_rand( m, 16 );
     test_aes_rand( k, 16 );
@@ -52,20 +54,27 @@ void test_aes( int n ) {
   for( int i = 0; i < n; i++ ) {
     uint8_t m[ 16 ], c[ 16 ], k[ 16 ];
 
+    printf("# aes dec %d / %d\n", i,n);
+
     test_aes_rand( c, 16 );
     test_aes_rand( k, 16 );
 
+
     #if defined( CONF_AES_PRECOMP_RK )  
+    printf("# var 1\n");
     uint8_t rk[ ( Nr + 1 ) * ( 4 * Nb ) ];
     aes_dec_exp( rk, k );
     aes_dec( m, c, rk, AES_DEC_SBOX, AES_MULX);
     #else
+    printf("# var 2\n");
     aes_dec( m, c,  k, AES_DEC_SBOX, AES_MULX);
     #endif
   
+    printf("# done 1\n");
     test_aes_dump( "c", c, 16 );
     test_aes_dump( "k", k, 16 );
     test_aes_dump( "m", m, 16 );
+    printf("# done 2\n");
   
     printf( "t = AES.new( k ).decrypt( c )                  " "\n" );
   
@@ -80,7 +89,7 @@ void test_aes( int n ) {
 }
 
 int main(int argc, char ** argv) {
-    test_aes(100);
+    test_aes(30);
     return 0;
 }
 
