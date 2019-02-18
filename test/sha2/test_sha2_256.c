eda0c923ccdf6713ab89b5f5aeb4706d37ba2170
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-02-18 08:46:23 +0000

    Adding SHA2 test.
    - SHA2 is broken.
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   test/Makefile.in
    	new file:   test/sha2/test_sha2_256.c
    
     Changes not staged for commit:
    	modified:   src/sha2/riscv-xcrypto/sha2_256_opt.S
    	modified:   src/sha2/sha2_256.h

---
+++ test/sha2/test_sha2_256.c
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "scarv/sha2/sha2_256.h"
+
+const uint32_t LEN = 32;
+
+//! Print the state to stdout as a hex string.
+void sha2_256_dump_state(uint8_t state[LEN]) {
+    uint8_t * p = (uint8_t*)state;
+
+    for(int i = 0; i < LEN; i ++) {
+        int k = LEN-i;
+        uint8_t c = p[i];
+        printf("%02X", c);
+    }
+}
+
+int main(int argc, char ** argv) {
+
+    uint8_t data_in  [LEN];
+    uint8_t data_out [LEN];
+
+    for(int i =0; i < LEN; i++){
+        data_in[i] = 1;
+    }
+    
+    printf("import hashlib\n");
+    printf("import binascii\n");
+    printf("import sys\n");
+
+    for(int i = 0; i < 5; i ++) {
+    
+        printf("din  = binascii.a2b_hex('");
+        sha2_256_dump_state(data_in );
+        printf("')\n");
+
+        sha2_256(data_out, 1, data_in, LEN);
+
+        printf("dout = '");
+        sha2_256_dump_state(data_out);
+        printf("'\n");
+
+        printf("golden = hashlib.sha256(din).hexdigest().upper()\n");
+        printf("if (golden != dout):\n");
+        printf("    print('Expected: ')\n");
+        printf("    print(golden)\n");
+        printf("    print('Got     :')\n");
+        printf("    print(dout)\n\n");
+
+        for(int i = 0; i < LEN; i ++) {
+            data_in[i] = data_out[i];
+        }
+
+    }
+
+    return 0;
+}
