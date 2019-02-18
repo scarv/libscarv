c056a6d4c6a9aea4b93f71309ab1d0c75f9b8486
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-02-18 11:16:27 +0000

    Add SHA512 test ready for implementing XC SHA512
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   test/Makefile.in
    	new file:   test/sha2/test_sha2_512.c

---
+++ test/sha2/test_sha2_512.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "scarv/sha2/sha2_512.h"
+
+const uint32_t LEN = 64;
+
+//! Print the state to stdout as a hex string.
+void sha2_512_dump_state(uint8_t * state, uint32_t l) {
+    uint8_t * p = (uint8_t*)state;
+
+    for(int i = 0; i < l; i ++) {
+        int k = l-i;
+        uint8_t c = p[i];
+        printf("%02X", c);
+    }
+}
+
+int main(int argc, char ** argv) {
+
+    uint8_t data_in  [LEN];
+    uint8_t data_out [LEN];
+
+    for(int i =0; i < LEN; i++){
+        data_in[i] = i;
+    }
+    
+    printf("import hashlib\n");
+    printf("import binascii\n");
+    printf("import sys\n");
+
+    for(int i = 0; i < 5; i ++) {
+    
+        printf("din  = binascii.a2b_hex('");
+        sha2_512_dump_state(data_in,LEN);
+        printf("')\n");
+
+        sha2_512(data_out, 1, data_in, LEN);
+
+        printf("dout = '");
+        sha2_512_dump_state(data_out,LEN);
+        printf("'\n");
+
+        printf("golden = hashlib.sha512(din).hexdigest().upper()\n");
+        printf("if (golden != dout):\n");
+        printf("    sys.stdout.write('Input   : ')\n");
+        printf("    print(binascii.b2a_hex(din)   )\n");
+        printf("    print('Expected: ')\n");
+        printf("    print(golden)\n");
+        printf("    print('Got     :')\n");
+        printf("    print(dout)\n\n");
+
+        for(int i = 0; i < LEN; i ++) {
+            data_in[i] = data_out[i];
+        }
+
+    }
+
+    return 0;
+}
