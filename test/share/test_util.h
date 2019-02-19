dd04def6db4695123323639c67a27f7216f8d2c0
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-02-19 16:11:48 +0000

    Test instrumentation code.
     On branch master
     Your branch is ahead of 'origin/master' by 2 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   test/share/test_util.h

---
+++ test/share/test_util.h
@@ -0,0 +1,38 @@
+
+#include <stdint.h>
+
+#ifndef TEST_UTIL_H
+#define TEST_UTIL_H
+
+
+//! Sample the clock cycle counter (used for timing checks)
+uint32_t test_util_rdcycle() {
+    uint32_t tr;
+    #if defined LIBSCARV_ARCH_RISCV
+        asm volatile ("rdcycle %0":"=r"(tr));
+    #elif defined LIBSCARV_ARCH_RISCV_XCRYPTO
+        asm volatile ("rdcycle %0":"=r"(tr));
+    #elif defined LIBSCARV_ARCH_GENERIC
+        tr = 0;
+    #else
+        tr = 0;
+    #endif
+    return tr;
+}
+
+//! Sample the clock cycle counter (used for timing checks)
+uint32_t test_util_rdinstret() {
+    uint32_t tr;
+    #if defined LIBSCARV_ARCH_RISCV
+        asm volatile ("rdinstret %0":"=r"(tr));
+    #elif defined LIBSCARV_ARCH_RISCV_XCRYPTO
+        asm volatile ("rdinstret %0":"=r"(tr));
+    #elif defined LIBSCARV_ARCH_GENERIC
+        tr = 0;
+    #else
+        tr = 0;
+    #endif
+    return tr;
+}
+
+#endif
