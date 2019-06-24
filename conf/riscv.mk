37bd91c8a1189d6a56d1afd797f9ec38c310bc4e
Dan Page <dan@phoo.org>
2019-06-24 10:56:59 +0100

    bump

---
+++ conf/riscv.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2019 SCARV project <info@scarv.org>
+#
+# Use of this source code is restricted per the MIT license, a copy of which 
+# can be found at https://opensource.org/licenses/MIT (or should be included 
+# as LICENSE.txt within the associated archive or repository).
+
+ifndef RISCV
+  $(error "point RISCV environment variable at toolchain installation")
+endif
+
+# =============================================================================
+
+export ARCH_SUBSET        = rv32imac
+export ARCH_ABI           = ilp32
+
+export TOOL_PREFIX_TARGET = riscv32-
+export TOOL_PREFIX_VENDOR = unknown-
+export TOOL_PREFIX_ABI    = elf-
+
+export TOOL_PREFIX        = ${RISCV}/bin/${TOOL_PREFIX_TARGET}${TOOL_PREFIX_VENDOR}${TOOL_PREFIX_ABI}
+
+export TEST_PREFIX        = 
+export TEST_SUFFIX        = 
+
+export CC_PATHS           =
+export CC_FLAGS           = -Wall -O3 -march=${ARCH_SUBSET} -mabi=${ARCH_ABI} 
+export CC_LIBS            =
+
+# =============================================================================
