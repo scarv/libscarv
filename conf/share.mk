3d21436c0d3ee4a0e4f33ccb7a5ee09f595bb66d
Dan Page <dan@phoo.org>
2019-08-23 11:40:13 +0100

    try to debug Travis build: can't use a branch, because it's branch specific :-/

--- conf/share.mk
+++ conf/share.mk
@@ -15,6 +15,10 @@ endif
 ifeq "${HOST}" "docker"
 
 define docker
+  echo $${DOCKER_REPO}
+  echo $${DOCKER_TAG}
+  echo $${DOCKER_FLAGS}
+  git branch
   docker run --rm --volume $${REPO_HOME}:/mnt/scarv/libscarv --env DOCKER_GID="$(shell id --group)" --env DOCKER_UID="$(shell id --user)" --env REPO_HOME="/mnt/scarv/libscarv" --env HOST="native" --env ARCHS="$${ARCH}" --env KERNELS="$${KERNELS}" $${DOCKER_FLAGS} $${DOCKER_REPO}:$${DOCKER_TAG} ${1}
 endef
 
